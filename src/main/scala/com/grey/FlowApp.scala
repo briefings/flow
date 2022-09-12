abfa297a0d43cdaff9994a0110f6858ae5b02c9d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-12 21:59:03 +0100

    Update src/main/scala/com/grey/FlowApp.scala

--- src/main/scala/com/grey/FlowApp.scala
+++ src/main/scala/com/grey/FlowApp.scala
@@ -5,9 +5,6 @@ import com.grey.environment.{DataDirectories, LocalSettings}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
-import scala.collection.parallel.immutable.ParSeq
-import scala.util.Try
-
 object FlowApp {
 
   private val localSettings = new LocalSettings()
@@ -28,12 +25,20 @@ object FlowApp {
       .getOrCreate()
 
 
+    // Spark logs
+    spark.sparkContext.setLogLevel("ERROR")
+
+
     // Directories
     List(localSettings.dataDirectory, localSettings.warehouseDirectory).par.map(
       directory => dataDirectories.localDirectoryReset(directory)
     )
 
-    val dataConfiguration = new DataConfiguration()
+
+    // Data
+    val dataConfiguration: DataConfiguration = new DataConfiguration()
+    new ImportData(api = dataConfiguration.api, dateTimePattern = dataConfiguration.dateTimePattern)
+      .importData(listOfDates = dataConfiguration.listOfDates)
 
   }
 
