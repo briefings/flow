9642e06cc9dfbfa3a7874f77ecfd1de549002b76
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-12 21:13:15 +0100

    Add src/main/scala/com/grey/functions/TimeBoundaries.scala

---
+++ src/main/scala/com/grey/functions/TimeBoundaries.scala
@@ -0,0 +1,26 @@
+package com.grey.functions
+
+import org.joda.time.DateTime
+
+import scala.util.Try
+import scala.util.control.Exception
+
+class TimeBoundaries {
+
+  def timeBoundaries(from: DateTime, until: DateTime): Boolean = {
+
+    // Set-up from & until comparison
+    val sequential: Try[Boolean] = Exception.allCatch.withTry(
+      from.isBefore(until) || from.isEqual(until)
+    )
+
+    // If comparable, ascertain 'from' precedes 'until'
+    if (sequential.isSuccess) {
+      if (sequential.get) sequential.get else sys.error("The start date must precede the end date")
+    } else {
+      sys.error(sequential.failed.get.getMessage)
+    }
+
+  }
+
+}
