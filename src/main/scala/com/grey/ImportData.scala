26797c68eeb4a897d5ae9a4eaa243cab36e664f9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-12 21:17:13 +0100

    Add src/main/scala/com/grey/ImportData.scala

---
+++ src/main/scala/com/grey/ImportData.scala
@@ -0,0 +1,14 @@
+package com.grey
+
+import org.apache.spark.sql.SparkSession
+import org.joda.time.DateTime
+
+class ImportData(spark: SparkSession) {
+
+  def importData(listOfDates: List[DateTime]): Unit = {
+
+
+
+  }
+
+}
