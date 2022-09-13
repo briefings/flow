5212f217e7ffd62ad5bd8cdda93152680808ceae
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-13 15:51:36 +0100

    Update src/main/scala/com/grey/functions/TimeSeries.scala

--- src/main/scala/com/grey/functions/TimeSeries.scala
+++ src/main/scala/com/grey/functions/TimeSeries.scala
@@ -2,8 +2,20 @@ package com.grey.functions
 
 import org.joda.time.DateTime
 
+
+/**
+ * Creates time series
+ */
 class TimeSeries {
 
+  /**
+   *
+   * @param from: Starting
+   * @param until: Ending
+   * @param step: Granularity
+   * @param stepType: the granularity's unit of measure, e.g., days, months, etc.
+   * @return
+   */
   def timeSeries(from: DateTime, until: DateTime, step: Int, stepType: String): List[DateTime] = {
 
     val timeSeriesIterator: Iterator[DateTime]  = stepType match {
