543f7b5bbb2876bc76a3364e4c1cd7ccbe62fbfb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-13 23:36:03 +0100

    Update src/main/scala/com/grey/Algorithms.scala

--- src/main/scala/com/grey/Algorithms.scala
+++ src/main/scala/com/grey/Algorithms.scala
@@ -1,25 +1,31 @@
 package com.grey
 
-import com.grey.configurations.DataSchema
+import com.grey.configurations.{DataConfiguration, DataSchema}
+import com.grey.environment.LocalSettings
+import com.grey.estimates.CumulativeDepartures
 import com.grey.functions.ScalaCaseClass
-import org.apache.spark.sql.expressions.{Window, WindowSpec}
+import org.apache.commons.io.FilenameUtils
 import org.apache.spark.sql.types.DateType
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
-import org.apache.spark.sql.functions.{count, sum}
 import org.apache.spark.storage.StorageLevel
+
+import java.nio.file.Paths
 
 
 /**
  *
- * @param spark: An instance of SparkSession
+ * @param spark : An instance of SparkSession
  */
 class Algorithms(spark: SparkSession) {
 
   private val dataSchema = new DataSchema(spark = spark)
+  private val cumulativeDepartures = new CumulativeDepartures(spark = spark)
+  private val dataConfiguration = new DataConfiguration()
+  private val localSettings = new LocalSettings()
 
   /**
    *
-   * @param dataStrings: A list of a data set's Paths.get(path, file name + file extension) strings
+   * @param dataStrings : A list of a data set's Paths.get(path, file name + file extension) strings
    */
   def algorithms(dataStrings: Array[String]): Unit = {
 
@@ -31,11 +37,6 @@ class Algorithms(spark: SparkSession) {
      */
     import spark.implicits._
 
-
-    // A window calculation specification
-    val windowSpec: WindowSpec = Window.partitionBy($"start_station_id")
-      .orderBy($"start_date".asc_nulls_last)
-      .rowsBetween(Window.unboundedPreceding, Window.currentRow)
 
     dataStrings.par.foreach { dataString =>
 
@@ -51,30 +52,30 @@ class Algorithms(spark: SparkSession) {
       // Using spark Dataset[]
       val baseline: Dataset[Row] = addStarting.as(
         ScalaCaseClass.scalaCaseClass(schema = addStarting.schema)).persist(StorageLevel.MEMORY_ONLY)
+      baseline.show(5)
 
 
       // Examples
-      baseline.groupBy($"start_date")
-        .agg(count("*").as("daily_departures"))
-        .orderBy($"start_date".asc_nulls_last)
-        .show(5)
+      cumulativeDepartures.cumulativeDepartures(baseline = baseline)
 
-      baseline.groupBy($"start_station_id", $"start_date")
-        .agg(count("*").as("daily_station_departures"))
-        .orderBy($"start_station_id".asc_nulls_last, $"start_date".asc_nulls_last)
-        .show(5)
 
-      baseline.select($"start_station_id", $"start_date")
-        .rollup($"start_station_id", $"start_date")
-        .agg(count("*").as("daily_station_departures"))
-        .orderBy($"start_station_id".asc_nulls_last, $"start_date".asc_nulls_last)
-        .show(5)
+      // Writing
+      println(FilenameUtils.getBaseName(Paths.get(dataString).getParent.toString))
+      println(FilenameUtils.getBaseName(dataString))
 
-      baseline.groupBy($"start_station_id", $"start_date")
-        .agg(count("*").as("outward"))
-        .select($"start_station_id", $"start_date",
-          sum($"outward").over(windowSpec).as("daily_station_departures_c"))
-        .show(5)
+
+      /*
+      Exception.allCatch.withTry(
+        baseline.coalesce(1).write
+          .option("header", value = true)
+          .option("encoding", value = "UTF-8")
+          .option("timestampFormat", value = dataConfiguration.sourceTimeStamp)
+          .option("dateFormat", value = "yyy-MM-dd")
+          .csv(path = Paths.get(localSettings.warehouseDirectory).toString)
+
+      )
+      */
+
 
     }
 
